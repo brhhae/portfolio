cf6ce5a538aa67bdfcdd9dff8ada4f9254c1798c
brhhae <mihriban.seydatar@cit.edu.al>
2022-10-26 18:15:28 +0200

    UPDATE: resume

--- src/components/contact/Contact.jsx
+++ src/components/contact/Contact.jsx
@@ -3,6 +3,17 @@ import { LinkedIn, Mail, Person } from '@mui/icons-material'
 
 const Contact = () => {
 
+  const handleClick = () => {
+    fetch('Resume.pdf').then(response => {
+        response.blob().then(blob => {
+            const fileURL = window.URL.createObjectURL(blob);
+            let alink = document.createElement('a');
+            alink.href = fileURL;
+            alink.download = 'Resume.pdf';
+            alink.click();
+        })
+    })
+}
 
   return (
     <div className="contact" id="contact">
@@ -11,7 +22,7 @@ const Contact = () => {
       </div>
       <div className="right">
         <h2>Contact.</h2>
-        <a href="https://github.com/brhhae/portfolio/blob/master/public/Resume.pdf">Download Resume</a>
+        <button onClick={handleClick}>Download Resume</button>
         <div className="info">
           <div className="itemContainer"><Person className="icon"/><span> +383 49 345 506</span></div>
           <div className="itemContainer"><Mail className="icon"/><span> m.seydatatar@gmail.com</span></div>
