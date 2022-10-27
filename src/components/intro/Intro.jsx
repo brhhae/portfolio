82ffc41b8e8fd557f550dadd10a8cc6d53d8fddf
brhhae <mihriban.seydatar@cit.edu.al>
2022-10-27 11:41:32 +0200

    UPDATE: responsive

--- src/components/intro/Intro.jsx
+++ src/components/intro/Intro.jsx
@@ -18,7 +18,7 @@ const Intro = () => {
     <div className='intro' id='intro'>
       <div className="left">
         <div className="imgContainer">
-          <img src="assets/sey.png" alt="" />
+          <img src="assets/sey2.png" alt="" />
         </div>
       </div>
       <div className="right">
